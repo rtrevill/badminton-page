7958ca7df18974877d97058777a44e0125d82151
rtrevill <139879271+rtrevill@users.noreply.github.com>
2025-11-21 15:24:06 +1100

    made enquiry form operational using Formspree

--- client/src/pages/ContactAndMap.jsx
+++ client/src/pages/ContactAndMap.jsx
@@ -1,26 +1,52 @@
+import {useForm, ValidationError } from '@formspree/react';
+
 export default function ContactAndMap () {
+
+    const [state, handleSubmit] = useForm("mqanvake");
+    if (state.succeeded){
+        return <p>Thankyou for your enquiry. It has been submitted and you will receive a reply shortly</p>
+    }
+
+    // const showall = () => {
+    //     event.preventDefault();
+    //     const evTar = event.target
+    //     console.log("Happy Days are here again", evTar.formMessage.value);
+    // }
+
+
+
     return (
         <div class="container-fluid row">
             <div class='col-5'>
-                <form>
-                    <div class="mb-3">
-                        <label for="formName" class="form-label">Name</label>
-                        <input type="text" class="form-control" id="formName" />
-                    </div>
-                    <div class="mb-3">
-                        <label for="exampleInputEmail1" class="form-label">Email address</label>
-                        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
-                        <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
-                    </div>
-                    <div class="mb-3">
-                        <label for="formSubject" class="form-label">Subject</label>
-                        <input type="text" class="form-control" id="formSubject" aria-describedby="subjectHelp" />
-                        <div id="subjectHelp" class="form-text">Please title your message</div>
-                    </div>
-                    <div class="mb-3">
-                        <label for="formMessage" class="form-label">Subject</label>
-                        <textarea style={{height: '300px'}} class="form-control" placeholder="Leave your message here" id="formMessage" />
-                    </div>
+                <form 
+                    onSubmit={handleSubmit}
+                >
+                    {/* <label>
+                        Your email:
+                        <input type="email" name="email" />
+                    </label>
+                    <label>
+                        Your message:
+                        <textarea name="message"></textarea>
+                    </label> */}
+                        <label for="formName" class="form-label">
+                            Name
+                        </label>
+                        <input type="text" class="form-control mb-3" name="formName" id="formName"/>
+                        <label for="exampleInputEmail1" class="form-label">
+                            Email address
+                        </label>
+                        <input type="email" class="form-control " id="exampleInputEmail1" name="exampleInputEmail1" aria-describedby="emailHelp" />
+                        <div id="emailHelp" class="form-text mb-3">We'll never share your email with anyone else.</div>
+                        <label for="formSubject" class="form-label">
+                            Subject
+                        </label>
+                        <input type="text" class="form-control " id="formSubject" name="formSubject" aria-describedby="subjectHelp" />
+                        <div id="subjectHelp" class="form-text mb-3">Please title your message</div>
+                        <label for="formMessage" class="form-label">
+                            Message
+                        </label>
+                        <textarea style={{height: '300px'}} class="form-control mb-3" placeholder="Leave your message here" id="formMessage" name="formMessage"/>
 
 
 
@@ -32,7 +58,7 @@ export default function ContactAndMap () {
                         <input type="checkbox" class="form-check-input" id="exampleCheck1" />
                         <label class="form-check-label" for="exampleCheck1">Check me out</label>
                     </div> */}
-                    <button type="submit" class="btn btn-primary">Submit</button>
+                    <button type="submit" class="btn btn-primary" disabled={state.submitting}>Submit</button>
                 </form>
             </div>
             <div class='col-5'>
